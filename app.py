322ea39ce6c4c39053a6e690c40949ef018e2124
HoangTruong0802 <ngaanthanhhoa@gmail.com>
2025-10-19 16:44:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def get_model(file_path):
     )
 
     # 4. Tạo Pipeline (Bao gồm Tiền xử lý + Mô hình Hồi quy)
-    model = DecisionTreeRegressor(random_state=42)
+    model = DecisionTreeRegressor(max_depth=3,random_state=42,min_samples_leaf = 2)
 
     pipeline = Pipeline(steps=[
         ('preprocessor', preprocessor),
@@ -178,4 +178,5 @@ if model_loaded:
 
     else:
         st.info("👈 Nhập thông tin ở thanh bên trái và nhấn nút 'Nhấn để Dự đoán'.")
+
 
